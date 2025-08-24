d6127d89c143446cbb5f857925ef3ceeb345f5da
Sanika Pandhare <pandharesanika62@gmail.com>
2025-08-24 11:16:26 +0530

    Add factorial.java program

---
+++ Factorial.java
@@ -0,0 +1,22 @@
+public class Factorial {
+    public static void  fact(int n)
+    {
+        if(n==1||n==0)
+        {
+            return 1;
+        }
+        else
+        {
+            return n* fact(n-1);
+        }
+    }
+    public static void main(String args[])
+    {
+        int n=5;
+       int result = fact (n);
+       System.out.println(result);
+
+        
+    }
+    
+}
